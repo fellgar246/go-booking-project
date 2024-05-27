5d7f41e83b30e2ad7f127f1f2033cc0551105780
Luis Felipe Garcia <fellgar246@gmail.com>
2024-05-26 19:02:57 -0600

    first commit

---
+++ cmd/web/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/fellgar246/go-templates-app/pkg/config"
+	"github.com/fellgar246/go-templates-app/pkg/handlers"
+	"github.com/fellgar246/go-templates-app/pkg/render"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+// go run cmd/web/*.go
+// main is the main function
+func main() {
+
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/fellgar246/go-booking-project
+
+go 1.21.3
+
+require github.com/alexedwards/scs/v2 v2.8.0 // indirect
